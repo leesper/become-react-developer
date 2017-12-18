b8c94d18d5063aad6ace79c725f1e1a740396774
leesper <pascal7718@gmail.com>
2017-12-18 15:05:27 +0800

    add DeckList component

---
+++ P3_UdaciCards/components/DeckList.js
@@ -0,0 +1,82 @@
+import React from "react"
+import { Text, View, FlatList, ScrollView, StyleSheet } from "react-native"
+
+const testData = [
+  {
+    title: "udacicards",
+    numOfCards: 3
+  },
+  {
+    title: "new deck",
+    numOfCards: 0
+  },
+  {
+    title: "New deck 2",
+    numOfCards: 0
+  },
+  {
+    title: "New deck 3",
+    numOfCards: 0
+  },
+  {
+    title: "New deck 4",
+    numOfCards: 0
+  },
+  {
+    title: "New deck 5",
+    numOfCards: 0
+  },
+  {
+    title: "New deck 6",
+    numOfCards: 0
+  },
+  {
+    title: "New deck 7",
+    numOfCards: 0
+  },
+  {
+    title: "New deck 8",
+    numOfCards: 0
+  },
+  {
+    title: "New deck 9",
+    numOfCards: 0
+  }
+]
+
+const ListItem = ({title, numOfCards}) => (
+  <View style={styles.container}>
+    <Text style={styles.title}>{title}</Text>
+    <Text style={styles.subTitle}>{numOfCards} cards</Text>
+  </View>
+)
+
+const DeckList = (props) => (
+  <FlatList
+    data={testData}
+    keyExtractor={(item) => item.title}
+    renderItem={({item}) => <ListItem key={item.title} title={item.title} numOfCards={item.numOfCards} />}
+  />
+)
+
+styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    borderBottomWidth: 1,
+    borderBottomColor: "black",
+    paddingTop: 40,
+    paddingBottom: 40
+  },
+  title: {
+    fontSize: 40,
+  },
+  subTitle: {
+    fontSize: 20,
+    color: "gray"
+  }
+})
+
+export default DeckList
