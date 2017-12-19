6d1e534bc5c70875c119f87f326932026bff50da
leesper <pascal7718@gmail.com>
2017-12-19 12:01:07 +0800

    add Stack Navigator

--- P3_UdaciCards/App.js
+++ P3_UdaciCards/App.js
@@ -1,17 +1,14 @@
 "use strict"
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { TabNavigator } from "react-navigation"
+import { TabNavigator, StackNavigator } from "react-navigation"
 import NewDeck from './components/NewDeck'
 import DeckList from "./components/DeckList"
 import Deck from "./components/Deck"
 import Quiz from "./components/Quiz"
 import NewQuestion from "./components/NewQuestion"
 
-const DeckListScreen = () => ( <DeckList /> )
-const NewDeckScreen = () => ( <NewDeck /> )
-
-const navigatorConfig = {
+const tabNavConfig = {
   tabBarOptions: {
     labelStyle: {
       fontSize: 20,
@@ -23,27 +20,49 @@ const navigatorConfig = {
   }
 }
 
+const stackNavConfig = {
+  navigationOptions: {
+    headerTitleStyle: {
+      fontSize: 20
+    }
+  }
+}
+
 const MainScreen = TabNavigator({
-  "Decks": {
-    screen: DeckListScreen,
+  Decks: {
+    screen: DeckList,
     navigationOptions: {
       tabBarLabel: "DECKS"
     }
   },
-  "New Deck": {
-    screen: NewDeckScreen,
+  NewDeck: {
+    screen: NewDeck,
     navigationOptions: {
       tabBarLabel: "NEW DECK"
     }
   }
-}, navigatorConfig)
+}, tabNavConfig)
+
+const HomeScreen = StackNavigator({
+  Main: {
+    screen: MainScreen,
+    navigationOptions: {
+      title: "HOME"
+    }
+  },
+  Deck: {
+    screen: Deck,
+    navigationOptions: {
+      title: "DECK"
+    }
+  },
+}, stackNavConfig)
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={{flex: 1}}>
-        <View style={{height: 20}} />
-        <NewQuestion />
+        <HomeScreen />
       </View>
     )
   }
